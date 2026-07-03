cb8bf7f32c61e413f818e05c612ddb266768879c
Vijay Padiyar <vijaypadiyar433@gmail.com>
2026-07-03 10:44:39 +0530

    feat: initial project structure — Spring Boot + MySQL connected

---
+++ src/main/java/com/vijay/journalify/JournalifyApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.journalify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijay/journalify/JournalifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.journalify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
